1d634ede0d5982b6017a31ecd9b7f3f3233b4b57
healp-kr <whoaau@gmail.com>
2026-08-26 16:59:14 +0900

    ai practice

---
+++ 01-frontend/01_aiCoding.js
@@ -0,0 +1,16 @@
+// 두 수를 더하는 함수를 작성해줘. 함수 이름은 Add야.
+function add(a, b) {
+    return a + b;
+}
+
+//변결할 영역을 드래그해서 선택한 후Ctrl+I를 눌러서 수정 가능함
+
+
+
+// 두 수를 빼는 함수를 작성해
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+// Ctrl + Shift+Alr +L 로 질문창을 엽니다
+// 질문을 이어서 연속적으로 가능 -> 설명 요구에 좋음
